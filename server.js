1a01e22e42214884ef95c9fada887c0d122286d0
江茂琼 <jiangmaoqiong@lizhi.fm>
2022-02-14 11:55:57 +0800

    prepared for delopment

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
-
+const path = require('path')
 //Connect DATAbase
 connectDB()
 
 //Init Middelware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API Runing'))
+// app.get('/', (req, res) => res.send('API Runing'))
 
 //define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+//Serve static assets in produection 在生产中服务静态资产
+if (process.env.NODE_ENV === 'rpoduction') {
+  //set static folder 设置静态文件夹
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
